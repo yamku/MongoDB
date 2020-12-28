ce867deb7b0dd7e0f943944a366aa8b3ace08b1c
Karki <yamku202@gmail.com>
2020-12-28 12:55:02 +1100

    Added Mongoshell cmds

---
+++ Mongo Shell Commands.sh
@@ -0,0 +1,19 @@
+#Connecting to Atlas
+mongo "mongodb+srv://<username>:<password>@<cluster>.mongodb.net/admin"
+
+
+#Import Export (BSON vs JSON)
+mongodump --uri "mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies"
+
+mongoexport --uri="mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies" --collection=sales --out=sales.json
+
+mongorestore --uri "mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies"  --drop dump
+
+mongoimport --uri="mongodb+srv://<your username>:<your password>@<your cluster>.mongodb.net/sample_supplies" --drop sales.json
+
+#Find Command
+db.zips.find({"state": "NY"}).count()
+
+db.zips.find({"state": "NY", "city": "ALBANY"})
+
+db.zips.find({"state": "NY", "city": "ALBANY"}).pretty()
